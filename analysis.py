d0b60d3d328b489772f597be5382156b1fcaae8b
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-03 15:33:36 +0100

    Add files via upload
    I added 4 comments at the beginning of the file stating what the program will do.

--- analysis.py
+++ analysis.py
@@ -1,2 +1,7 @@
 # Author : Michael Allen
 # Analysis of Fisher's Iris Data Set
+#The program:
+#1.Outputs a summary of each variable to a single text file, 
+#2. Saves a histogram of each variable to png files, and 
+#3. Outputs a scatter plot of each pair of variables. 
+#4. Performs any other analysis i think is appropriate
