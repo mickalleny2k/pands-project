1aa183f27c52ec5af49c199b60ee2967843f5808
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-07 15:46:55 +0100

    Add files via upload
    Read in the Fisher's Iris dataset csv file

--- analysis.py
+++ analysis.py
@@ -5,3 +5,14 @@
 #2. Saves a histogram of each variable to png files, and 
 #3. Outputs a scatter plot of each pair of variables. 
 #4. Performs any other analysis i think is appropriate
+
+import csv
+
+filename = "fishers_iris_dataset.csv"
+
+with open(filename, 'rt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    for line in csv_reader:
+        print(line)
+
+        
