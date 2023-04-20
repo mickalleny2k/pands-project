d05371394bb268ae5ebc8da02e980f4fab6cfab0
Michael Allen <123869970+mickalleny2k@users.noreply.github.com>
2023-04-20 16:42:50 +0100

    Add files via upload
    I added the histogram code

--- analysis.py
+++ analysis.py
@@ -1,36 +1,34 @@
-# Author : Michael Allen
-# Analysis of Fisher's Iris Data Set
-#The program:
-#1.Outputs a summary of each variable to a single text file, 
-#2. Saves a histogram of each variable to png files, and 
-#3. Outputs a scatter plot of each pair of variables. 
-#4. Performs any other analysis i think is appropriate
-
 import csv
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import sys
+import matplotlib.pyplot as plt
+from sklearn import datasets
 
 filename = "iris.csv"
 
-iris_data = pd.read_csv('iris.csv')
-iris_data.columns = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'species']
+iris = pd.read_csv('iris.csv')
+iris.columns = ['sepal_length', 'sepal_width' , 'petal_length', 'petal_width', 'species']
 
-'''with open(filename, 'rt') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    for line in csv_reader:
-        print(line)
-'''
 sys.stdout = open('output.txt','wt')
-summary = iris_data.describe()
+summary = iris.describe()
 summary = summary.transpose()
 summary.head()
 print(summary)
 
-#sys.stdout = open('output.txt','wt')
+iris= datasets.load_iris()
 
-#pd.describe(csv_file)
-#print(describe)
+fig, axes = plt.subplots(nrows= 2, ncols=2)
+colors= ['blue', 'red', 'green']
 
-        
+for i, ax in enumerate(axes.flat):
+    for label, color in zip(range(len(iris.target_names)), colors):
+        ax.hist(iris.data[iris.target==label, i], label=             
+                            iris.target_names[label], color=color)
+        ax.set_xlabel(iris.feature_names[i])  
+        ax.legend(loc='upper right')
+        plt.savefig('Histogram.png')
+
+plt.show()
+plt.close()
